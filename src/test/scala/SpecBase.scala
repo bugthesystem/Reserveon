4161647f5c509822af347547ca8447527f835e8d
Ziya SARIKAYA <ziya.sarikaya@here.com>
2017-04-24 21:45:34 +0200

    extend SpecBase with CirceCommonCodecs (refactoring)

--- src/test/scala/SpecBase.scala
+++ src/test/scala/SpecBase.scala
@@ -1,26 +1,18 @@
 
-import java.sql.Timestamp
-
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import de.heikoseeberger.akkahttpcirce.FailFastCirceSupport
 import handlers.OAuth2DataHandler
 import http.HttpService
-import io.circe.Decoder.Result
-import io.circe.{ Decoder, Encoder, HCursor, Json }
+import io.circe.generic.auto._
+import io.circe.syntax._
 import org.scalatest.{ Matchers, WordSpec }
 import org.specs2.mock.Mockito
 import services._
-import io.circe.generic.auto._
-import io.circe.syntax._
+import utils.CirceCommonCodecs
 
-//TODO: before after
-trait SpecBase extends WordSpec with Matchers with ScalatestRouteTest with Mockito with FailFastCirceSupport {
-
-  implicit val TimestampFormat: Encoder[Timestamp] with Decoder[Timestamp] = new Encoder[Timestamp] with Decoder[Timestamp] {
-    override def apply(a: Timestamp): Json = Encoder.encodeLong.apply(a.getTime)
-
-    override def apply(c: HCursor): Result[Timestamp] = Decoder.decodeLong.map(s => new Timestamp(s)).apply(c)
-  }
+trait SpecBase extends WordSpec
+    with Matchers with ScalatestRouteTest with Mockito with FailFastCirceSupport
+    with CirceCommonCodecs {
 
   val databaseService: DatabaseService = mock[DatabaseService]
   val accountsService: AccountsService = mock[AccountsService]
