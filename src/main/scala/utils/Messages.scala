b918cd9c35a8f14993b4d25087a4a05261b87a53
ziλasal <sarikayaziya@gmail.com>
2017-05-07 13:32:31 +0300

    Update Messages.scala

--- src/main/scala/utils/Messages.scala
+++ src/main/scala/utils/Messages.scala
@@ -5,7 +5,7 @@ object Messages {
   trait Reservation {
     val RESERVATION_CREATED = "Reservation created"
     val RESERVATION_CREATE_NO_AVAILABLE_SEAT = "There is no available seat for this movie!"
-    val RESERVATION_CREATE_NO_AVAILABLE_MOVIE = "There is no available movie for this movie!"
+    val RESERVATION_CREATE_NO_AVAILABLE_MOVIE = "Requested movie does not exists!"
   }
 
   trait Movies {
